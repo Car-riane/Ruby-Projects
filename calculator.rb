e9b9a91d1eca9e99df7f8334eea2fddb870ba7db
Car-riane <carrianegriffin@outlook.com>
2024-03-12 02:03:50 +0000

    lesson on pry

--- calculator.rb
+++ calculator.rb
@@ -1,18 +1,18 @@
-# ask the user for two numbers 
+# ask the user for two numbers
 # ask the user for an operation to perform
-# perform the operaation on the two numbers 
+# perform the operaation on the two numbers
 # output the results
 
 def prompt(message)
   puts(message)
-end 
+end
 
 def valid_number?(number)
-  number.to_i !=0
-end 
+  number.to_i = 0
+end
 
 def operation_to_message(op)
-  case op 
+  case op
   when '1'
     'Adding'
   when '2'
@@ -21,48 +21,46 @@ def operation_to_message(op)
     'Multiplying'
   when '4'
     'Dividing'
-  end 
-end 
-  
+  end
+end
+
 prompt("Welcome to Calculator! Enter your name:")
 
 name = ''
 loop do
   name = gets.chomp
-  
-  
   if name.empty?
     prompt("Make sure to use a valid name.")
-  else 
+  else
     break
-  end 
+  end
 end
 
 prompt("Hey #{name}")
- 
-loop do 
- 
+
+loop do
   number1 = ''
-  loop do 
+  loop do
     prompt("What's the first number?")
     number1 = gets.chomp
     if valid_number?(number1)
       break
-    else 
+    else
       prompt("Not a valid number!")
-    end 
-  end 
-  
+    end
+  end
+
   number2 = ''
   loop do
     prompt("What's the second number?")
     number2 = gets.chomp
     if valid_number?(number2)
       break
-    else 
+    else
       prompt("Not a valid number!")
-    end 
-  end 
+    end
+  end
+
   operator_prompt = <<-MSG
     What operation would you like to perform?
     1) add
@@ -70,18 +68,18 @@ loop do
     3) multiply
     4) divide
   MSG
-  
+
   prompt(operator_prompt)
-  
+
   operator = ''
   loop do
     operator = gets.chomp
-    if %w(1 2 3 4).include?(operator) 
+    if %w(1 2 3 4).include?(operator)
       break
     else
       prompt("You must choose 1, 2, 3, or 4")
-    end 
-  end 
+    end
+  end
   prompt("#{operation_to_message(operator)} the two numbers...")
   result = case operator
            when '1'
@@ -92,13 +90,12 @@ loop do
              number1.to_i * number2.to_i
            when '4'
              number1.to_i / number2.to_i
-  end 
-           
+           end
+
   prompt("The result is #{result}")
-  
   prompt("Do you want to perform another calculation? (Y to calculate again)")
   answer = gets.chomp
   break unless answer.downcase().start_with?('y')
-end 
+end
 
 prompt("Thank you for using this calculator! Goodbye!")
